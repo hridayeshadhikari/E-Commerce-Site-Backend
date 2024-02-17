c37135cd4fd84f7a90cc43202a6aac14a6b7df73
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-02-17 22:52:15 +0530

    initial commit

---
+++ E-Commerce-Project/src/main/java/com/ecommerceproject/ECommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjectApplication.class, args);
+	}
+
+}
