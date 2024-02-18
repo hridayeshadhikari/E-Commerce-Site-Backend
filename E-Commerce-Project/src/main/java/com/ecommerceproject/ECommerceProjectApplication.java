47704aa3b6833d87da03581a10b4d4bfbfdccd8d
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-02-18 10:25:06 +0530

    initial 1

--- E-Commerce-Project/src/main/java/com/ecommerceproject/ECommerceProjectApplication.java
+++ E-Commerce-Project/src/main/java/com/ecommerceproject/ECommerceProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceProjectApplication.class, args);
+    }
 
 }
